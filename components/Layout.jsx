46aaf2574c7b6175baf6980d8cffd683594c2d1b
Marcel Balint <blank_blankness@yahoo.com>
2022-09-15 01:54:29 -0400

    Created the Navigation

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,22 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
-const Layout = ({ children }) => {
+//import Footer from './Footer';
+import Navbar from "./Navabr";
+
+export default function Layout({ children }) {
   return (
     <>
       <Head>
         <title>Real Estate</title>
       </Head>
       <Box maxWidth="1280px" m="auto">
-        <header>navbar</header>
+        <header>
+          <Navbar />
+        </header>
         <main>{children}</main>
-        <footer>footer</footer>
+        <footer>Footer</footer>
       </Box>
     </>
   );
-};
-
-export default Layout;
+}
