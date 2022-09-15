2574e9784cc4dc5e88352a4614bab92de5f6290a
Marcel Balint <blank_blankness@yahoo.com>
2022-09-15 00:58:35 -0400

    Created the Layout component and added ChakraProvider

--- pages/index.js
+++ pages/index.js
@@ -69,9 +69,11 @@ export default function Home(properties) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      {properties.propertiesForSale.map((property) => (
-        <Property property={property} key={property.id} />
-      ))}
+      <Flex flexWrap="wrap">
+        {properties.propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
     </Box>
   );
 }
